5ffa9d58fba4fc59804e4098bc60032fbbdcd74b
Nattick <natalia.a.konovalova@gmail.com>
2023-03-11 14:32:25 +0300

    Add Ex58

--- Ex58/Program.cs
+++ Ex58/Program.cs
@@ -7,5 +7,71 @@
 // 18 20
 // 15 18
 
-using static Shares;
+Console.WriteLine($"\nEnter matrix and random element range:");
+int m = InputNumbers("Enter 1st matrix rows number: ");
+int n = InputNumbers("Enter 1st matrix columns number: ");
+int r = InputNumbers("Enter 2nd matrix rows number: ");
+int p = InputNumbers("Enter 2nd matrix columns number: ");
+int range = InputNumbers("Enter random number range from 1 to ");
 
+int[,] firstMartrix = new int[m, n];
+CreateArray(firstMartrix);
+Console.WriteLine($"\nFirst matrix: ");
+WriteArray(firstMartrix);
+
+int[,] secomdMartrix = new int[r, p];
+CreateArray(secomdMartrix);
+Console.WriteLine($"\nSecond matrix: ");
+WriteArray(secomdMartrix);
+
+int[,] resultMatrix = new int[m,p];
+
+MultiplyMatrix(firstMartrix, secomdMartrix, resultMatrix);
+Console.WriteLine($"\nProduct of matrices:");
+WriteArray(resultMatrix);
+
+void MultiplyMatrix(int[,] firstMartrix, int[,] secomdMartrix, int[,] resultMatrix)
+{
+  for (int i = 0; i < resultMatrix.GetLength(0); i++)
+  {
+    for (int j = 0; j < resultMatrix.GetLength(1); j++)
+    {
+      int sum = 0;
+      for (int k = 0; k < firstMartrix.GetLength(1); k++)
+      {
+        sum += firstMartrix[i,k] * secomdMartrix[k,j];
+      }
+      resultMatrix[i,j] = sum;
+    }
+  }
+}
+
+int InputNumbers(string input)
+{
+  Console.Write(input);
+  int output = Convert.ToInt32(Console.ReadLine());
+  return output;
+}
+
+void CreateArray(int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      array[i, j] = new Random().Next(range);
+    }
+  }
+}
+
+void WriteArray (int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      Console.Write(array[i,j] + " ");
+    }
+    Console.WriteLine();
+  }
+}
