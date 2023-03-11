64b4c110a6f1bfb6075b3919b9743eae3c1e460c
Nattick <natalia.a.konovalova@gmail.com>
2023-03-11 14:19:25 +0300

    Add Ex54

--- Ex54/Program.cs
+++ Ex54/Program.cs
@@ -12,3 +12,59 @@
 
 using static Shares;
 
+
+int GetValueFromUser(string message)
+{
+    Console.WriteLine(message);
+    int value = Convert.ToInt32(Console.ReadLine());
+    return value;
+}
+
+void FillMatrix(int[,] matrix)
+{
+    for (int rows = 0; rows < matrix.GetLength(0); rows++)
+    {
+        for (int columns = 0; columns < matrix.GetLength(1); columns++)
+        {
+            matrix[rows, columns] = new Random().Next(1,100);
+        }
+    }
+}
+
+void OrderMaxToMin(int [,] matrix)
+{
+    for (int rows = 0; rows < matrix.GetLength(0); rows++) 
+    {   
+        for (int i = 0; i < matrix.GetLength(1); i++) 
+        {
+            for (int columns = 0; columns < matrix.GetLength(1) - 1; columns++)
+            {
+                if(matrix[rows, columns] < matrix[rows, columns + 1])
+                {
+                    int temp = matrix[rows, columns];
+                    matrix[rows, columns] = matrix[rows, columns + 1];
+                    matrix[rows, columns + 1] = temp;
+                } 
+            }
+        }
+    }
+}
+
+void PrintMatrix(int[,] matrix)
+{
+        for (int rows = 0; rows < matrix.GetLength(0); rows++)
+    {
+        Console.WriteLine("\n");
+        for (int columns = 0; columns < matrix.GetLength(1); columns++)
+        {
+            Console.Write($"{matrix[rows, columns]} | ");
+        }
+    }
+}
+
+int[,] matrix = new int[GetValueFromUser("Number of rows > "),
+                        GetValueFromUser("Number of columns > ")];
+
+FillMatrix(matrix);
+OrderMaxToMin(matrix);
+PrintMatrix(matrix);
